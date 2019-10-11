5cd86adb90b2978dcaa9ac02f1f53b61de4d7d61
Ionut Isac <ionut.isac@balfour.com>
2019-10-11 15:44:11 -0500

    Small test for homepage

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -14,8 +14,8 @@ class ExampleTest extends TestCase
     {
         $this->get('/');
 
-        $this->assertEquals(
-            $this->app->version(), $this->response->getContent()
-        );
+        $this->seeJson([
+        'status' => 200,
+    ]);
     }
 }
